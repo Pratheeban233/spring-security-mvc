7931d646a8a4967431c97c9ff6d6bf6e74f72a2c
mnprathi233@gmail.com <40929176+Pratheeban233@users.noreply.github.com>
2021-03-27 12:00:07 +0530

    passwordEncoder Bcrypt vs NoOp
    (cherry picked from commit bb3cd91c8114f6257d71b8e401a560c4ac908d1f)
    
    # Conflicts:
    #	src/main/java/com/spring/security/config/WebSecurityConfig.java

--- src/main/java/com/spring/security/config/WebSecurityConfig.java
+++ src/main/java/com/spring/security/config/WebSecurityConfig.java
@@ -1,37 +1,9 @@
 package com.spring.security.config;
 
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity(debug=true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth
-                .inMemoryAuthentication()
-                .withUser("Prathi")
-                .password("{bcrypt}$2a$10$rYgKaEwBXGqnPlCVzW7NaOf13GBIYtHmxmNmyzNWbgJ6peN.RPQMO")
-                .roles("Admin");
-
-    }
-	/*@Bean
-	public PasswordEncoder getPasscodeEncoder() {
-	
-		return NoOpPasswordEncoder.getInstance();
-		
-		//return new BCryptPasswordEncoder();
-	}*/
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.
-                authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and().httpBasic();
-    }
 }
