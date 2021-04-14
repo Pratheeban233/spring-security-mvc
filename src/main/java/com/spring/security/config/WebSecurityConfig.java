33238ee4b70e14ced395fdb777a6dd61d84c99b7
mnprathi233@gmail.com <40929176+Pratheeban233@users.noreply.github.com>
2021-04-14 16:03:39 +0530

    http basic authentication from intellij

--- src/main/java/com/spring/security/config/WebSecurityConfig.java
+++ src/main/java/com/spring/security/config/WebSecurityConfig.java
@@ -1,26 +1,23 @@
 package com.spring.security.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @EnableWebSecurity(debug = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		auth
-		.inMemoryAuthentication()
-		.withUser("Prathi")
-		.password("{bcrypt}$2a$10$rYgKaEwBXGqnPlCVzW7NaOf13GBIYtHmxmNmyzNWbgJ6peN.RPQMO")
-		.roles("Admin");
-		
-	}
-	
+        auth
+                .inMemoryAuthentication()
+                .withUser("Prathi")
+                .password("{bcrypt}$2a$10$rYgKaEwBXGqnPlCVzW7NaOf13GBIYtHmxmNmyzNWbgJ6peN.RPQMO")
+                .roles("Admin");
+
+    }
 	/*@Bean
 	public PasswordEncoder getPasscodeEncoder() {
 	
@@ -29,4 +26,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		//return new BCryptPasswordEncoder();
 	}*/
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.
+                authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and().httpBasic();
+    }
 }
